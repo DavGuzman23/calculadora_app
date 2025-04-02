a5bd46b002c7b10e3aaf470aaf99fb483d3054a9
David Guzmán Roldán <gummer2310@gmail.com>
2025-04-02 08:13:01 +0200

    Add hidden in result

--- src/components/CalculatorDisplay.js
+++ src/components/CalculatorDisplay.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const CalculatorDisplay = ({ result, operation, currentNumber }) => {
     return (
-        <div className="calculator-display">
-            <span>{result}</span>
-            {operation && <span className="operation">{operation}</span>}
-            {currentNumber && <span>{currentNumber}</span>}
+        <div className="calculator-display" role="textbox" aria-label="Resultado de la calculadora">
+            <span title={result}>{result}</span>
+            {operation && <span className="operation" title={`Operador: ${operation}`}>{operation}</span>}
+            {currentNumber && <span title={currentNumber}>{currentNumber}</span>}
         </div>
     );
 };
